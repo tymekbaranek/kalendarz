1fe04c2c74bb1ce7b5cef9b0c2bcf685b4a3dde7
tymekbaranek <tymekbaranek@wp.pl>
2022-12-20 21:38:40 +0100

    commit 20.12. brak zaznaczenia

--- app/src/main/java/com/example/kalendarz/MainActivity.kt
+++ app/src/main/java/com/example/kalendarz/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.kalendarz
 
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
@@ -9,12 +8,10 @@ import android.widget.CalendarView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.alpha
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.ZoneId
 import java.util.*
-import kotlin.math.absoluteValue
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
@@ -32,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         kalendarz.minDate = System.currentTimeMillis()
         kalendarz.maxDate = LocalDate.now().plusYears(2).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
-        kalendarz.setFirstDayOfWeek(Calendar.MONDAY);
+        kalendarz.setFirstDayOfWeek(Calendar.MONDAY)
 
         val dataPocz = mutableListOf<Int>(0,0,0)
         val dataKon = mutableListOf<Int>(0,0,0)
@@ -48,14 +45,23 @@ class MainActivity : AppCompatActivity() {
             dataPocz[0] = date[0]
             dataPocz[1] = date[1]
             dataPocz[2] = date[2]
-            poczOut.text = "Początek wyjazdu: " +date[0].toString()+"-"+date[1].toString()+"-"+date[2].toString();
-            kalendarz.selectedDateVerticalBar
+            poczOut.text = "Początek wyjazdu: " +date[0].toString()+"-"+date[1].toString()+"-"+date[2].toString()
+
+            if(dataPocz[0] != 0 && dataKon[0] != 0)
+                if(dataPocz[0] > dataKon[0] || dataPocz[2] > dataKon[2] && dataPocz[1] == dataKon[1])
+                    dlugosc.text = "Koniec wyjazdu nie może być wcześniej niż początek"
+                else{
+                    val temp1 = (dataKon[0]*360) + (dataKon[1]*30) + dataKon[2]
+                    val temp2 = (dataPocz[0]*360) + (dataPocz[1]*30) + dataPocz[2]
+                    val temp = temp1.toChar() - temp2.toChar()
+                    dlugosc.text = "Wyjazd trwa "+temp.toString()+" dni"}
+
         }
         koniec.setOnClickListener {
             dataKon[0] = date[0]
             dataKon[1] = date[1]
             dataKon[2] = date[2]
-            konOut.text = "Koniec wyjazdu: " +date[0].toString()+"-"+date[1].toString()+"-"+date[2].toString();
+            konOut.text = "Koniec wyjazdu: " +date[0].toString()+"-"+date[1].toString()+"-"+date[2].toString()
 
             if(dataPocz[0] != 0 && dataKon[0] != 0)
                 if(dataPocz[0] > dataKon[0] || dataPocz[2] > dataKon[2] && dataPocz[1] == dataKon[1])
